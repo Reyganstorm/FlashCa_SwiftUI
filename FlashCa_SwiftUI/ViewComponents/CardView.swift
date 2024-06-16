858a687fe4544bbb80b7b78f966b8e738b6b97c5
Ruslan Shtybaev <79254522+Reyganstorm@users.noreply.github.com>
2024-06-16 21:53:58 +0300

    UPDATE: - empty

---
+++ FlashCa_SwiftUI/ViewComponents/CardView.swift
@@ -0,0 +1,19 @@
+//
+//  CardView.swift
+//  FlashCa_SwiftUI
+//
+//  Created by Руслан Штыбаев on 16.06.2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+    }
+}
+
+#Preview {
+    CardView()
+}
