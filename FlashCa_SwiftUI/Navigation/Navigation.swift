a73ff6138b29e146dbba08db9fa6c627742893ab
Ruslan Shtybaev <79254522+Reyganstorm@users.noreply.github.com>
2024-06-28 22:56:36 +0300

    ADD: - navigation list

---
+++ FlashCa_SwiftUI/Navigation/Navigation.swift
@@ -0,0 +1,19 @@
+//
+//  Navigation.swift
+//  FlashCa_SwiftUI
+//
+//  Created by Руслан Штыбаев on 28.06.2024.
+//
+
+import Foundation
+
+
+/// Open start view:
+///
+///  Select view:
+///  - Settings
+///  - Edit
+///  - Premium
+///  - Logs
+///  - Privacy Policy
+///  - Terms of Use
