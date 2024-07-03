30c8fbf88733516a56a92a278b018d68b50881cb
Ruslan Shtybaev <79254522+Reyganstorm@users.noreply.github.com>
2024-07-03 21:29:52 +0300

    ADD: - mainScreenView

---
+++ FlashCa_SwiftUI/ScreenViews/MainSreenView.swift
@@ -0,0 +1,18 @@
+//
+//  MainSreenView.swift
+//  FlashCa_SwiftUI
+//
+//  Created by Руслан Штыбаев on 03.07.2024.
+//
+
+import SwiftUI
+
+struct MainSreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainSreenView()
+}
