da920d85194e89260d72e5558d75c071c86146bb
Ruslan Shtybaev <79254522+Reyganstorm@users.noreply.github.com>
2024-08-07 22:56:55 +0300

    CREATE: - SettingsScreenView

---
+++ FlashCa_SwiftUI/Core/SettingsScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsScreenView.swift
+//  FlashCa_SwiftUI
+//
+//  Created by Руслан Штыбаев on 07.08.2024.
+//
+
+import SwiftUI
+
+struct SettingsScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsScreenView()
+}
